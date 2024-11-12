bcc7819982d1f9e8bb363d3b76ae2411253c55cd
lucan8 <gamer.lucan72@gmail.com>
2024-11-12 10:35:09 +0200

    removed

--- DAW/project/exceptions/FileWriteException.php
+++
@@ -1,9 +0,0 @@
-<?php
-class FileWriteException extends Exception{
-    private $file_name;
-    public function __construct($file_name){
-        $this->file_name = $file_name;
-        parent::__construct("Could not write to file: " . $this->file_name);
-    }
-}
-?>
