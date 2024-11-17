c6f6d30e1b477950d1f66f968112c1a28810ada8
lucan8 <gamer.lucan72@gmail.com>
2024-11-17 11:23:50 +0200

    moved everything from index to layout

---
+++ DAW/hosplive/app/views/layout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/index.css">
+    <title>Home</title>
+</head>
+<body>
+    <div id="layout_container">
+        <header id="layout_header">
+            <h1>Welcome to Hosplive</h1>
+            <nav id="layout_nav">
+                <a href="index">Home</a>
+                <a href="make_appointment">Make Appointments</a>
+                <a href="hospital_list">Our Hospitals</a>
+                <a href="contact_us">Your Appointments</a>
+                <a href="about_us">About Us</a>
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
