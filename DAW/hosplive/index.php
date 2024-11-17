21fbd85b45e3f1d756816971c2230a179f8a4a51
lucan8 <gamer.lucan72@gmail.com>
2024-11-17 11:24:34 +0200

    now sets the connection using entity

--- DAW/hosplive/index.php
+++ DAW/hosplive/index.php
@@ -1,5 +1,9 @@
 <?php
     require_once "config/router.php";
+    require_once "app/models/Entity.php";
+
+    if (!Entity::isConnectionSet())
+        Entity::setConnection();
 
     $router = new Router();
     $router->direct();
