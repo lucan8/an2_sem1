b5d95e776f8df757065ebd5cea3897c0c8eb52f0
lucan8 <gamer.lucan72@gmail.com>
2024-11-20 22:16:08 +0200

    added getById

--- DAW/hosplive/app/models/Counties.php
+++ DAW/hosplive/app/models/Counties.php
@@ -11,8 +11,20 @@
     }
 
     class Counties extends Entity{
-        // public static function getCounties(){
-        //     return [new CountiesData(1, "Nairobi"), new CountiesData(2, "Mombasa"), new CountiesData(3, "Kisumu"), new CountiesData(4, "Nakuru")];
-        // } 
+        public static function getCounties(){
+            return [new CountiesData(1, "Nairobi"), new CountiesData(2, "Mombasa"), new CountiesData(3, "Kisumu"), new CountiesData(4, "Nakuru")];
+        }
+
+        public static function getById(int $county_id): CountiesData{
+            $query = "SELECT * FROM " . static::class . " WHERE county_id = ?";
+            self :: printQuery($query, [$county_id]);
+
+            $stm = self :: $conn->prepare($query);
+            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            
+            $stm->execute([$county_id]);
+
+            return $stm->fetch();
+        }
     }
 ?>
