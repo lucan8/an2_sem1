eb49f49bbd2ddcf32de98e790f3565d5aa10830a
lucan8 <gamer.lucan72@gmail.com>
2024-11-20 22:16:50 +0200

    added way to retrieve all specialization from a hospital

--- DAW/hosplive/app/models/Specializations.php
+++ DAW/hosplive/app/models/Specializations.php
@@ -14,6 +14,20 @@
         // static public function getSpecializations() : array{
         //     return ["cardiology", "neurology", "orthopedics", "pediatrics", "oncology", "gynecology", "urology", "dermatology", "ophthalmology", "psychiatry"];
         // }
+
+        //Returns all specializations present in hospital given by argument
+        static public function getHospitalSpecialization($hospital_id): array{
+            $query = "SELECT s.specialization_id, s.specialization_name
+                      FROM " . static::class . " s JOIN medics m ON m.specialization_id = s.specialization_id
+                      JOIN hospitals_medics hm ON hm.medic_id = m.medic_id
+                      WHERE hm.hospital_id = ?";
+            self::printQuery($query, [$hospital_id]);
+            $stm = self :: $conn->prepare($query);
+            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            $stm->execute([$hospital_id]);
+
+            return $stm->fetchAll();
+        }
         //TO DO: Remove query1 and just return success
     }
 ?>
