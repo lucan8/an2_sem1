37f1637d12af5374d9d8016fc8fc180586769d4a
lucan8 <gamer.lucan72@gmail.com>
2024-11-20 22:15:15 +0200

    added printQuery and a general _insert

--- DAW/hosplive/app/models/Entity.php
+++ DAW/hosplive/app/models/Entity.php
@@ -1,7 +1,7 @@
 <?php
 abstract class EntityData{}
 abstract class Entity{
-    protected static $conn = null;
+    protected static PDO|null $conn = null;
 
     public static function isConnectionSet(): bool{
         return self::$conn != null;
@@ -13,16 +13,41 @@ abstract class Entity{
         self::$conn = &$pdo;
     }
 
-    public static function insert(EntityData $data): string{
-        $query = "INSERT INTO " . static::class . " VALUES(";
-        $query .= implode(", ", array_values(get_object_vars($data))) . ');';
-        return $query;
+    public static function printQuery(string $query, array $params = []){
+        if (count($params) != 0){
+            $placeholders = array_fill(0, count($params), '?');
+            str_replace($placeholders, $params, $query);
+        }
+        print($query);
+    }
+
+    protected static function _insert(EntityData $data){
+        //Setting the data as array and the placeholders(filtering out null values)
+        $data_array = array_filter(get_object_vars($data));
+        $inserted_values = array_values($data_array);
+        $placeholders = '?' . str_repeat(', ?', count($data_array) - 1);
+
+        //Creating the query
+        $query = "INSERT INTO " . static::class . "(" .
+                  implode(", ", array_keys($data_array)) .
+                  ") VALUES(" . $placeholders. ")";
+        
+
+        //Print substituted query for debug
+        self :: printQuery($query, $inserted_values);
+
+        //Executing the query
+        $stm = self::$conn->prepare($query);
+        $stm->execute(array_values($inserted_values));
     }
 
     public static function getAll(): array{
         $query = "SELECT * FROM " . static::class;
+        self::printQuery($query);
+
         $stm = self::$conn->prepare($query);
         $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+        
         $stm->execute();
         return $stm->fetchAll();
     }
