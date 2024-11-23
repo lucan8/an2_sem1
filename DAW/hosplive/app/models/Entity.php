ea8de682ad05919dcb496ad45d6640b2e0ba50ec
lucan8 <gamer.lucan72@gmail.com>
2024-11-23 13:44:15 +0200

    added query_log

--- DAW/hosplive/app/models/Entity.php
+++ DAW/hosplive/app/models/Entity.php
@@ -1,24 +1,46 @@
 <?php
+require_once "exceptions/FileOpenException.php";
 abstract class EntityData{}
 abstract class Entity{
+    private const QUERY_LOG_FILE = "query_log.txt";
     protected static PDO|null $conn = null;
+    protected static $log_file = null; 
+
+    //Should be called only once before any other method
+    public static function init(){
+        if (self::isConnectionSet() || self::isQueryLogSet())
+            return;
+        self::setConnection();
+        self::setLogFile();
+    }
 
     public static function isConnectionSet(): bool{
         return self::$conn != null;
     }
 
-    public static function setConnection(){
+    public static function isQueryLogSet(): bool{
+        return self::$log_file != null;
+    }
+
+    private static function setConnection(){
         require_once 'config/pdo.php';
         $pdo = &$pdo;
         self::$conn = &$pdo;
     }
 
+    private static function setLogFile(){
+        self::$log_file = fopen(self::QUERY_LOG_FILE, "w");
+        if (self::$log_file == false)
+            throw new FileOpenException(self::QUERY_LOG_FILE);
+    }
+
     public static function printQuery(string $query, array $params = []){
         if (count($params) != 0){
             $placeholders = array_fill(0, count($params), '?');
-            str_replace($placeholders, $params, $query);
+            $query = str_replace($placeholders, $params, $query);
         }
-        print($query);
+         
+        fwrite(self::$log_file, date("d/m/yy H:i:s: ") . $query . "\n");
     }
 
     protected static function _insert(EntityData $data){
