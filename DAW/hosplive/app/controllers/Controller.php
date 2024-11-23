9530092c052268e3dade59bbd0b6e3c979ad2346
lucan8 <gamer.lucan72@gmail.com>
2024-11-23 22:05:53 +0200

    added more get routes for make_appointment

--- DAW/hosplive/app/controllers/Controller.php
+++ DAW/hosplive/app/controllers/Controller.php
@@ -5,19 +5,44 @@ class Controller {
     }
 
     public static function makeAppointment() {
-        require_once "app/models/Counties.php";
-        //$counties = json_encode(Counties::getAll());
-        $counties = json_encode(Counties::getCounties());
-        require_once "app/models/Specializations.php";
-        //$specializations = json_encode(Specializations::getAll());
-        $specializations = json_encode(Specializations::getSpecializations());
-        require_once "app/views/make_appointment.php";
+        if ($_SERVER["REQUEST_METHOD"] == "GET"){
+            require_once "app/models/Counties.php";
+            //$hospitals = json_encode(Hospitals::getHospitalsAndCounties());
+            $counties = json_encode(Counties::getAll());
+            require_once "app/models/Specializations.php";
+            //$specializations = json_encode(Specializations::getAll());
+            $specializations = json_encode(Specializations::getAll());
+            require_once "app/models/Hospitals.php";
+            require_once "app/models/Appointments.php";
+            require_once "app/views/make_appointment.php";
+
+        }
+        // else if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        //     require_once "app/models/Appointments.php";
+        //     require_once "app/models/Hospital.php";
+
+        //     $hospital_id = Hospitals::getByCounty($_POST["county_id"])->id;
+        //     new AppointmentsData(null, $hospital_id, $_POST["medic_id"], $_POST["room_id"],
+        //                          $_POST["appointment_date"], $_POST["appointment_time"], null);
+        //     Appointments::insert($data);
+        // }
     }
 
+    //TO DO: Get medics from chosen hospital with a given specialization passed to _GET
     public static function getMedics() {
+        require_once "app/models/Hospitals.php";
+        $chosen_hospital = (Hospitals::getByCounty((int)$_GET["county_id"]))->hospital_id;
+        
         require_once "app/models/Medics.php";
-        $doctors = json_encode(Medics::getMedics());
-        echo $doctors;
+        $medics = Medics::getByHospAndSpec($chosen_hospital, (int)$_GET["spec_id"]);
+        $res = ["medics" => $medics, "chosen_hospital" => $chosen_hospital];
+        echo json_encode($res);
+    }
+
+    public static function getFreeTimeIntervals() {
+        require_once "app/models/Appointments.php";
+        $time = json_encode(Appointments::getFreeTimeIntervals($_GET["hospital_id"], $_GET["medic_id"], $_GET["appointment_date"]));
+        echo $time;
     }
 }
 ?>
