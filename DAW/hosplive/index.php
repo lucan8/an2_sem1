233b5a4745d4b99ec19618d20a3e9886832565ed
lucan8 <gamer.lucan72@gmail.com>
2024-11-25 11:14:33 +0200

    using only init

--- DAW/hosplive/index.php
+++ DAW/hosplive/index.php
@@ -2,8 +2,11 @@
     require_once "config/router.php";
     require_once "app/models/Entity.php";
 
-    if (!Entity::isConnectionSet())
-        Entity::setConnection();
+    //Run this before:
+    //cd "C:\Program Files\MySQL\MySQL Server 8.4\bin"
+    //mysqld --defaults-file="C:\ProgramData\MySQL\MySQL Server 8.4\my.ini" --init-file="C:\mysql_init.txt" --console
+    //This resets the password everytime
+    Entity::init();
 
     $router = new Router();
     $router->direct();
