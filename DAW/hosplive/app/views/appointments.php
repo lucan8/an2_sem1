58af0fdd34e9ac0b3530c64df81d3c60acf04b6b
lucan8 <gamer.lucan72@gmail.com>
2024-12-09 10:49:06 +0200

    date and time are more like selects now, added hidden input fields for hospital_id and medic_id

--- DAW/hosplive/app/views/appointments.php
+++ DAW/hosplive/app/views/appointments.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../../public/appointments.css?$$REVISION$$" rel="stylesheet">
-    <script src="../../public/appointments.js?$$REVISION$$"></script>
+    <script src="../../public/appointment.js?$$REVISION$$"></script>
     <title>Your Appointments</title>
+
+    <div id="constants_appointments" hidden>
+        <option id="opening_time_appointments" value="<?= Hospitals :: OPENING_TIME ?>"><?= Hospitals :: OPENING_TIME ?></option>
+        <option id="closing_time_appointments" value="<?= Hospitals :: CLOSING_TIME ?>"><?= Hospitals :: CLOSING_TIME ?></option>
+        <option id="app_duration_appointments" value="<?= Appointments :: DEFAULT_DURATION ?>"><?= Appointments :: DEFAULT_DURATION ?></option>
+    </div>
 </head>
 
 <body>
@@ -15,12 +21,17 @@
                 echo "<div class='appointment_cont' id='" . $appointment["id"] . "'>";
                 echo "<div>Hospital Location<p>" . $appointment["county"] . "</p></div>";
                 echo "<div>Medic<p>" . $appointment["medic_name"] . "</p></div>";
-                echo "<div>Date<p contenteditable='true' id='date_" . $appointment["id"] . "'>" . $appointment["date"] . "</p></div>";
-                echo "<div>Time<p contenteditable='true' id='time_" . $appointment["id"] . "'>" . $appointment["time"] . "</p></div>";
+                echo "<div>Date<input class='app_date' type='date' id='date_" . $appointment["id"] . "' value='". $appointment["date"] . "'> </div>";
+                //The option is temporary, after all times are loaded this will be removed
+                //It will be replaced with a copy that is placed in it's normal place(sorted asc)
+                echo "<div>Time<select class='app_time' id='time_" . $appointment["id"] . "'><option selected value='" . $appointment["time"] . "'>" . $appointment["time"] . "</option></select></div>";
                 echo "<div>Room<p>" . $appointment["room"] . "</p></div>";
                 echo "<button class='cancel_app'>Cancel</button>";
                 echo "<button class='edit_app'>Save Changes</button>";
-                echo "</div>";   
+                //Hidden inputs to store the ids
+                echo "<input type='hidden' id='medic_id_" . $appointment["id"] . "' value='" . $appointment["medic_id"] . "'>";
+                echo "<input type='hidden' id='hospital_id_" . $appointment["id"] . "'value='" . $appointment["hospital_id"] . "'>";
+                echo "</div>";
             }?>
     </div>
 </body>
