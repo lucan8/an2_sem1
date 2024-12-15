9830ef7a90c47aad75031d013594dad449224c38
lucan8 <gamer.lucan72@gmail.com>
2024-12-15 12:54:21 +0200

    added

---
+++ DAW/hosplive/app/controllers/AbstractController.php
@@ -0,0 +1,10 @@
+<?php
+interface AbstractController{
+    public static function index();
+    public static function add();
+    public static function get();
+    public static function remove();
+    public static function edit();
+    public static function getConstants();
+}
+?>
