3aef82e3c45989f20ed88adadad43d9ff46ef708
lucan8 <gamer.lucan72@gmail.com>
2024-12-19 14:09:15 +0200

    added logout button

--- DAW/hosplive/app/views/index.php
+++ DAW/hosplive/app/views/index.php
@@ -1,3 +1,11 @@
-<?php
-    require_once "layout.php";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+
+<body>
+    <form method="post" action="auth/logout">
+        <input type='submit' name='submit' value='Logout'>
+    </form>
+</body>
