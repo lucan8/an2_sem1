575b66125310c6f698d3776e3efd683e7fb1600e
lucan8 <gamer.lucan72@gmail.com>
2024-12-19 14:07:42 +0200

    now checks if the user is logged in before serving the page

--- DAW/hosplive/app/controllers/Controller.php
+++ DAW/hosplive/app/controllers/Controller.php
@@ -1,6 +1,9 @@
 <?php
+require_once "app/controllers/AuthController.php";
 class Controller {
     public static function index() {
+        AuthController::checkLogged();
+        require_once "app/views/layout.php";
         require_once "app/views/index.php";
     }
 }
