50f38739289688329b906353225c2687dd5ca118
lucan8 <gamer.lucan72@gmail.com>
2024-12-28 20:29:12 +0200

    Added view for medics' job applications

---
+++ DAW/hosplive/app/views/job/medic_applications.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Job Applications</title>
+        <link rel="stylesheet" type="text/css" href="../../public/appointments.css">
+    </head>
+
+    <body>
+        <h1>These are your sent job applications</h1>
+        <?php
+            foreach ($applications as $application){
+                echo "<div class='appointment_cont' id='application_" . $application["id"] . "'>";
+                echo "<p>Hospital Location: " . $application["county"]. "</p>";
+                echo "<p>Application Date: " . $application["date"] . "</p>";
+                echo "<p>Status: " . $application["status"] . "</p>";
+                echo "</div>";
+            }
+        ?>
+    </body>
+
+</html>
