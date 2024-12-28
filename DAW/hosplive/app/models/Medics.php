c56b738bf1499102dce8f4e335a756f11deb1710
lucan8 <gamer.lucan72@gmail.com>
2024-12-28 21:20:27 +0200

    added way to retrieve id column and now the medic name is retrieved from users

--- DAW/hosplive/app/models/Medics.php
+++ DAW/hosplive/app/models/Medics.php
@@ -6,9 +6,10 @@
         public int|null $specialization_id;
         public int|null $years_exp;
 
-        function __construct(int $user_id = null, int $specialization_id = null, int $years_exp = null){
+        function __construct(int $user_id = null, $medic_id = null,
+                             int $specialization_id = null, int $years_exp = null){
             parent::__construct($user_id);
-
+            $this->medic_id = $medic_id;
             $this->specialization_id = $specialization_id;
             $this->years_exp = $years_exp;
         }
@@ -20,7 +21,7 @@
             self::printQuery($query, [$spec_id]);
 
             $stm = self :: $conn->prepare($query);
-            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            $stm->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, static::class . "Data");
 
             $stm->execute([$spec_id]);
             return $stm->fetchAll();
@@ -28,21 +29,27 @@
 
         //Returns an array of medics from a given hospital with a given specialization
         public static function getByHospAndSpec($hosp_id, $spec_id): array{
-            $query = "SELECT m.medic_name, m.medic_id, m.specialization_id, m.years_exp
+            $query = "SELECT u.last_name || ' ' || u.first_name as medic_name, m.medic_id, m.specialization_id, m.years_exp
                       FROM " . static::class . " m JOIN hospitals_medics hm ON m.medic_id = hm.medic_id
+                      JOIN users u ON m.user_id = u.user_id
                       WHERE hm.hospital_id = ? AND m.specialization_id = ?";
 
             self::printQuery($query, [$hosp_id, $spec_id]);
             
             $stm = self::$conn->prepare($query);
-            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            $stm->setFetchMode(PDO::FETCH_ASSOC);
             
             $stm->execute([$hosp_id, $spec_id]);
             
             return $stm->fetchAll();
         }
+
         public static function getNeccesaryRows(): array{
             return ['specialization_id', 'years_exp'];
+        }
+
+        public static function getIdColumn(){
+            return 'medic_id';
         }
     }
 ?>
