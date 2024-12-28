f2cb28eda20f21fd7c15676fef2b1de0eccacc45
lucan8 <gamer.lucan72@gmail.com>
2024-12-28 21:22:22 +0200

    added way to retrieve id column and necessary rows

--- DAW/hosplive/app/models/Pacients.php
+++ DAW/hosplive/app/models/Pacients.php
@@ -3,14 +3,21 @@
     class PacientsData extends AbstractUserData{
         //Auto increment keys are set to 0 by default so that they are ignored when inserting
         //Should be read only but the fetch mode is set to FETCH_CLASS which sets the properties directly
-        public int $pacient_id = 0;
+        public int|null $pacient_id = 0;
 
-        function __construct($user_id = null){
+        function __construct($user_id = null, int $pacient_id = null){
             parent::__construct($user_id);
+            $this->pacient_id = $pacient_id;
         }
     }
 
     class Pacients extends AbstractUser{
-        
+        public static function getIdColumn(){
+            return 'pacient_id';
+        }
+
+        public static function getNeccesaryRows(): array{
+            return [];
+        }
     }
 ?>
