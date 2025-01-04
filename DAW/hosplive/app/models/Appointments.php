bef03d6b9b8c72b627e0903e6e24c7cede6cb01f
lucan8 <gamer.lucan72@gmail.com>
2025-01-04 19:46:56 +0200

    Now fetching appointments by patient

--- DAW/hosplive/app/models/Appointments.php
+++ DAW/hosplive/app/models/Appointments.php
@@ -1,23 +1,24 @@
 <?php
     require_once 'Entity.php';
     class AppointmentsData extends EntityData{
-        //Auto increment(auto increment keys are set to 0 by default so that they are ignored when inserting)
-        public int $appointment_id = 0;
+        //Auto increment keys are set to 0 by default so that they are ignored when inserting
+        //Should be read only but the fetch mode is set to FETCH_CLASS which sets the properties directly
+        public int|null $appointment_id = 0;
 
-        public int $user_id;
-        public int $hospital_id;
-        public int $medic_id;
-        public int $room_id;
+        public int|null $patient_id;
+        public int|null $hospital_id;
+        public int|null $medic_id;
+        public int|null $room_id;
         
-        public string $appointment_date;
-        public string $appointment_time;
-        public int $duration;
+        public string|null $appointment_date;
+        public string|null $appointment_time;
+        public int|null $duration;
 
-        function __construct(){}
-        public function set(int $user_id, int $hospital_id, int $medic_id, int $room_id,
-                            string $appointment_date, string $appointment_time,
-                            int $duration = Appointments :: DEFAULT_DURATION){
-            $this->user_id = $user_id;
+        function __construct(int $appointment_id = null, int $patient_id = null, int $hospital_id = null,
+                             int $medic_id = null, int $room_id = null, string $appointment_date = null,
+                             string $appointment_time = null, int $duration = Appointments :: DEFAULT_DURATION){
+            $this->appointment_id = $appointment_id;
+            $this->patient_id = $patient_id;
             $this->hospital_id = $hospital_id;
             $this->medic_id = $medic_id;
             $this->room_id = $room_id;
@@ -30,60 +31,54 @@
     class Appointments extends Entity{
         // Default duration of an appointment in minutes
         public const DEFAULT_DURATION = 30;
-
-        public static function getByUser($user_id): array{
-            $query = "SELECT * FROM " . static::class . " WHERE user_id = ?";
-            self::printQuery($query, [$user_id]);
-
-            $stm = self::$conn->prepare($query);
-            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
-            
-            $stm->execute([$user_id]);
-
-            return $stm->fetchAll();
-        }
         
         //Returns appointments data substituting the ids with the names
-        public static function getAppointments($user_id): array{
+        public static function getAppointmentsByPatient(int $patient_id): array{
             $query = "SELECT a.appointment_id as id, c.county_name as county,
-                             m.medic_name, m.medic_id, h.hospital_id, a.appointment_date as date,
-                             a.appointment_time as time, a.room_id as room, a.duration
+                             CONCAT(u.last_name, ' ', u.first_name) as medic_name, m.medic_id, h.hospital_id,
+                             a.appointment_date as date, a.appointment_time as time,
+                             a.room_id as room, a.duration
                       FROM " . static::class . " a
                       JOIN hospitals h ON a.hospital_id = h.hospital_id
                       JOIN medics m ON a.medic_id = m.medic_id
                       JOIN counties c ON h.county_id = c.county_id
-                      WHERE a.user_id = ? ORDER BY date, time";
-            self::printQuery($query, [$user_id]);
+                      JOIN users u ON m.medic_id = u.user_id
+                      WHERE a.patient_id = ? ORDER BY date, time";
+            self::printQuery($query, [$patient_id]);
 
             $stm = self::$conn->prepare($query);
-            $stm->execute([$user_id]);
+            $stm->execute([$patient_id]);
 
             return $stm->fetchAll();
         }
 
         //Gets the first free room for a given hospital, date and time
-        public static function getFreeRoom($hospital_id, $appointment_date, $appointment_time): AppointmentsData|false{
+        //If none is found throw error
+        public static function getFreeRoom(int $hospital_id, string $appointment_date, string $appointment_time): AppointmentsData{
             $query = "SELECT room_id FROM rooms WHERE hospital_id = ? EXCEPT
                       SELECT room_id FROM " . static::class .
                       " WHERE hospital_id = ? AND appointment_date = ? AND appointment_time = ? LIMIT 1";
             self::printQuery($query, [$hospital_id, $hospital_id, $appointment_date, $appointment_time]);
 
             $stm = self::$conn->prepare($query);
-            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            $stm->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, static::class . "Data");
             
             $stm->execute([$hospital_id, $hospital_id, $appointment_date, $appointment_time]);
 
-            return $stm->fetch();
+            $room = $stm->fetch();
+            if ($room === false)
+                throw new Exception("No free room found for the given hospital, date and time");
+            return $room;
         }
         
-        public static function getByHospMedDate($hospital_id, $medic_id, $appointment_date): array{
+        public static function getByHospMedDate(int $hospital_id, int $medic_id, string $appointment_date): array{
             // Getting the appointments made at the given hospital, medic and date ordered by time
             $query = "SELECT * FROM " . static::class . " WHERE hospital_id = ? AND medic_id = ?
                       AND appointment_date = ? ORDER BY appointment_time";
             self::printQuery($query, [$hospital_id, $medic_id, $appointment_date]);
 
             $stm = self::$conn->prepare($query);
-            $stm->setFetchMode(PDO::FETCH_CLASS, static::class . "Data");
+            $stm->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, static::class . "Data");
             
             $stm->execute([$hospital_id, $medic_id, $appointment_date]);
 
@@ -104,18 +99,21 @@
             return $success;
         }
         
-        public static function removeById(int $id): bool{
+        public static function removeById(int $appointment_id): bool{
             $query = "DELETE FROM " . static::class . " WHERE appointment_id = ?";
-            self::printQuery($query, [$id]);
+            self::printQuery($query, [$appointment_id]);
     
             $stm = self::$conn->prepare($query);
-            $success = $stm->execute([$id]);
+            $success = $stm->execute([$appointment_id]);
     
             //Checking if exactly one row was affected
             $affectred_rows = $stm->rowCount();
             if ($affectred_rows != 1)
                 throw new AffectedRowsException($affectred_rows, 1);
             return $success;
+        }
+        public static function getIdColumn(): string{
+            return 'appointment_id';
         }
     }
 ?>
