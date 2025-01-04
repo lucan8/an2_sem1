61add915fc1b96c4f74380834cf05a7067ab54d5
lucan8 <gamer.lucan72@gmail.com>
2025-01-04 22:26:37 +0200

    using absolute routes

--- DAW/hosplive/app/views/layout.php
+++ DAW/hosplive/app/views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/index.css">
+    <link rel="stylesheet" href="../../public/css/index.css">
     <title>Home</title>
 </head>
 <body>
@@ -11,8 +11,8 @@
         <header id="layout_header">
             <h1>Welcome to Hosplive</h1>
             <nav id="layout_nav">
-                <a href="../index">Home</a>
-                <a href="appointments">Appointments</a>
+                <a href="/hosplive/index">Home</a>
+                <a href="/hosplive/appointments/appointments">Appointments</a>
                 <a href="about_us">About Us</a>
             </nav>
         </header>
