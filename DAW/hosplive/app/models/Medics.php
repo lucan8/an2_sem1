a57b283b66e02cb11eb0015fa863527a2b4e7216
lucan8 <gamer.lucan72@gmail.com>
2025-01-04 19:42:55 +0200

    Fixed method that gets medics from a certain hospital and that have a certain specialization
    Now job_applications with the 'Hired' status will be the substitute of hospitals_medics and Medics no longer extends AbstractUser, as the medic_id and user_id merged into one

--- DAW/hosplive/app/models/Medics.php
+++ DAW/hosplive/app/models/Medics.php
@@ -1,22 +1,19 @@
 <?php
-    require_once 'AbstractUser.php';
-    class MedicsData extends AbstractUserData{
-        //Auto increment(auto increment keys are set to 0 by default so that they are ignored when inserting)
-        public int|null $medic_id = 0;
+    require_once 'Entity.php';
+    class MedicsData extends EntityData{
+        public int|null $medic_id;
         public int|null $specialization_id;
         public int|null $years_exp;
 
-        function __construct(int $user_id = null, $medic_id = null,
-                             int $specialization_id = null, int $years_exp = null){
-            parent::__construct($user_id);
+        function __construct($medic_id = null, int $specialization_id = null, int $years_exp = null){
             $this->medic_id = $medic_id;
             $this->specialization_id = $specialization_id;
             $this->years_exp = $years_exp;
         }
     }
 
-    class Medics extends AbstractUser{
-        public static function getMedicsBySpec($spec_id): array{
+    class Medics extends Entity{
+        public static function getMedicsBySpec(int $spec_id): array{
             $query = "SELECT * FROM " . static::class . " WHERE specialization_id = ?";
             self::printQuery($query, [$spec_id]);
 
@@ -28,11 +25,14 @@
         }
 
         //Returns an array of medics from a given hospital with a given specialization
-        public static function getByHospAndSpec($hosp_id, $spec_id): array{
-            $query = "SELECT u.last_name || ' ' || u.first_name as medic_name, m.medic_id, m.specialization_id, m.years_exp
-                      FROM " . static::class . " m JOIN hospitals_medics hm ON m.medic_id = hm.medic_id
-                      JOIN users u ON m.user_id = u.user_id
-                      WHERE hm.hospital_id = ? AND m.specialization_id = ?";
+        public static function getByHospAndSpec(int $hosp_id, int $spec_id): array{
+            //Complicated for now, will be simplified after medics and hospitals will use only user_ids
+            $query = "SELECT concat(u.last_name,' ', u.first_name) as medic_name, m.medic_id, m.specialization_id, m.years_exp
+                      FROM " . static::class . " m
+                      JOIN users u ON m.medic_id = u.user_id
+                      JOIN job_applications ja ON u.user_id = ja.applicant_id
+                      WHERE ja.hirer_id = ? AND m.specialization_id = ?
+                      AND ja.application_status_id = (SELECT application_status_id FROM application_statuses WHERE application_status_name = 'Hired')";
 
             self::printQuery($query, [$hosp_id, $spec_id]);
             
@@ -48,7 +48,7 @@
             return ['specialization_id', 'years_exp'];
         }
 
-        public static function getIdColumn(){
+        public static function getIdColumn(): string{
             return 'medic_id';
         }
     }
