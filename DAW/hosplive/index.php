d242f5bbbaf8bc0c93073bd724a58003b700d50e
lucan8 <gamer.lucan72@gmail.com>
2025-01-04 19:28:53 +0200

    setting auth  controller relevant data

--- DAW/hosplive/index.php
+++ DAW/hosplive/index.php
@@ -1,13 +1,16 @@
 <?php
     require_once "config/router.php";
+    require_once "app/controllers/AuthController.php";
     require_once "app/models/Entity.php";
 
     //Run this before:
     //cd "C:\Program Files\MySQL\MySQL Server 8.4\bin"
     //mysqld --defaults-file="C:\ProgramData\MySQL\MySQL Server 8.4\my.ini" --init-file="C:\mysql_init.txt" --console
     //This resets the password everytime
+    //Initialization part
     Entity::init();
-
+    AuthController::setAuth();
+    
     $router = new Router();
     $router->direct();
 ?>
