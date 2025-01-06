03d136b94a89e250ab1b95f933b303037e2ec40a
lucan8 <gamer.lucan72@gmail.com>
2025-01-06 17:25:18 +0200

    Setting the CSP header to load content only from this server and google's recaptcha

--- DAW/hosplive/index.php
+++ DAW/hosplive/index.php
@@ -11,6 +11,10 @@
     Entity::init();
     AuthController::setAuth();
     
+    //Only load from this server and google recaptcha
+    //Should be set differently on each view depending on what is needed
+    header("Content-Security-Policy: default-src 'self'; script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/releases/zIriijn3uj5Vpknvt_LnfNbF/recaptcha__ro.js; style-src 'self'; img-src 'self'; connect-src 'self'; font-src 'self'; frame-src 'self' https://www.google.com/recaptcha/;");
+    
     $router = new Router();
     $router->direct();
 ?>
