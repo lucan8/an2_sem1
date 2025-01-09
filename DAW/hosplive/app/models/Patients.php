c5aee011dd46edec436e4011b29ca210d438f457
lucan8 <gamer.lucan72@gmail.com>
2025-01-09 15:01:59 +0200

    added way to retrieve the patient name

--- DAW/hosplive/app/models/Patients.php
+++ DAW/hosplive/app/models/Patients.php
@@ -9,6 +9,19 @@
     }
 
     class Patients extends Entity{
+        public static function getPatientName(int $pacient_id): string|false{
+            $query = "SELECT CONCAT(u.last_name, ' ', u.first_name) as patient_name FROM " . self::class . " p
+                      JOIN Users u ON u.user_id = p.patient_id
+                      WHERE patient_id = ?";
+            self::printQuery($query, [$pacient_id]);
+
+            $stm = self::$conn->prepare($query);
+
+            $stm->execute([$pacient_id]);
+
+            return $stm->fetchColumn();
+
+        }
         public static function getIdColumn(): string{
             return 'patient_id';
         }
